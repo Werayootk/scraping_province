3e42bcfedb1fc5353473364a3e18745bf21e3551
Werayootk <werayoot5800@gmail.com>
2022-01-09 11:17:23 +0700

    add note

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+Event flow
+- https://www.drivehub.co/
+- Select id=province-container-ref > class=card province-item > id=province-item-*
+- Select class=province-item-header [grab content header]> click > 
+- Select class=province-item-location show > each class=location-item [grab to array]
+
+Type Car
+- https://search.drivehub.co/?booking_begin=2022-01-27%2010%3A00&booking_end=2022-01-29%2010%3A00&dealers=&from=from_homepage&location_id=205&sort_by=price
+- class= category-type flex-center flex-column > p > span
+
+Data Car
+- 
+
+Save Picture Split folder
+- gallery__item > style = url
+
+note
+- https://www.codegrepper.com/code-examples/javascript/javascript+download+image+from+url (script download image from url)
+- https://www.scrapingbee.com/blog/infinite-scroll-puppeteer/ 
+- https://stackoverflow.com/questions/63905776/how-to-get-style-background-img-url-using-cheerio (script grab url from tag style)
+- https://www.scrapingbee.com/blog/download-file-puppeteer/ (puppeteer download file)
+- https://stackoverflow.com/questions/51529332/puppeteer-scroll-down-until-you-cant-anymore (script scroll down auto puppeteer)
+- https://javascript.plainenglish.io/scraping-for-images-using-puppeteer-9a3700bd5a2d (keep link to json data)
+- https://www.py4u.net/discuss/338163 (save all img web)
+- https://dev.to/microworlds/image-downloader-with-puppeteer-and-the-fetch-api-5b8e 
+- https://stackoverflow.com/questions/52542149/how-can-i-download-images-on-a-page-using-puppeteer
+- https://www.freecodecamp.org/news/how-to-scrape-websites-with-node-js-and-cheerio/
+- https://tkssharma.com/scrape-web-with-nodejs/
+- https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial
+- https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
+*/
+
